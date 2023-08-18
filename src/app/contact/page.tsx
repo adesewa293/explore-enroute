dfe4e6c512855dc70271c34c34f9647ec5ee66c2
Adesewa Aishat <adesewaaishat@gmail.com>
2023-08-18 03:01:22 +0100

    add simple contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,28 @@
 export default function ContactPage() {
   return (
-  <p>this it the contact page</p>
-  )
+    <div className="mb-10">
+      <h2 className="text-3xl font-bold mb-4 underline underline-offset-8">
+        Contact me
+      </h2>
+      <p>
+        If you'd like to get in touch, feel free to connect with me through the
+        following channels:
+      </p>
+
+      <ul>
+        <li>
+          <a href="https://github.com/adesewa293">GitHub</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/adesewa-adedokun-461945172/">
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          Email:{" "}
+          <a href="mailto:adesewaaishat@gmail.com">adesewaaishat@gmail.com</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
