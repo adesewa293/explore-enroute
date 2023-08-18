2af2c3b1a5196328c44594c660ee28ac90641911
Adesewa Aishat <adesewaaishat@gmail.com>
2023-08-18 02:37:16 +0100

    add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,10 @@
 export default function Footer() {
   return (
-    <footer>Footer</footer>
-  )
+    <footer className="mt-8 mb-10">
+      <p className="text-center">
+        <span className="font-medium">&copy; 2023 Explore Enroute. </span>
+        Every mile is a memory &#127757;
+      </p>
+    </footer>
+  );
 }
