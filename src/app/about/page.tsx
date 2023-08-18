07488fec046d7722645303503155c415b93a7f61
Adesewa Aishat <adesewaaishat@gmail.com>
2023-08-18 02:59:16 +0100

    add about me page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,63 @@
 export default function AboutPage() {
   return (
-    <div>About page</div>
-  )
+    <div>
+      <h2 className="text-3xl font-bold mb-4 underline underline-offset-8">
+        About me
+      </h2>
+      <p className="mb-2 text-lg">
+        Hello fellow explorers! My name is Adesewa, and while I may be a junior
+        software engineer by profession, my heart beats for the thrill of travel
+        and discovery. Dive into "Explore Enroute" and journey with me through
+        the vibrant tapestry of destinations, cultures, and stories waiting to
+        be shared.
+      </p>
+      <p className="mb-2 text-lg">
+        Starting my coding journey, I immersed myself in the world of
+        JavaScript, React, and Next.js. Just as learning a new language opens up
+        pathways to communicate and connect with diverse cultures, coding opened
+        up a realm where I could express my creativity, and my passion for
+        travel in a digital format. And thus, "Explore Enroute" was born – a
+        delightful intersection of technology and my travel passion.
+      </p>
+
+      <p className="mb-2 text-lg">
+        Much like coding, travel has its own set of challenges and puzzles.
+        Deciphering a city's metro map, adapting to a new timezone, or even just
+        trying to order food in a language you barely understand – these are the
+        problems that travelers love to solve. And it’s these intricate details,
+        these shared human experiences, that I aim to capture in my blog.
+      </p>
+
+      <p className="mb-2 text-lg">
+        In "Explore Enroute", I chronicle my adventures, share insightful travel
+        tips, and showcase mesmerizing destinations. But more than just a travel
+        diary, this blog is a testament to my journey as a software engineer.
+        Each pixel you see, every transition on the pages, and every
+        user-friendly feature has been meticulously crafted by me, blending my
+        technical expertise with my love for exploration.
+      </p>
+
+      <p className="mb-2 text-lg">
+        If you're a tech enthusiast, I invite you to appreciate the fluidity of
+        React components and the seamless routing capabilities of Next.js that
+        make this blog not just informative, but also a joy to navigate. If
+        you're here for travel, prepare to be enthralled by tales from bustling
+        city streets, tranquil beach sunsets, and the myriad experiences in
+        between.
+      </p>
+
+      <p className="mb-2 text-lg">
+        So whether you're a coding novice, a seasoned traveler, or someone who
+        simply stumbled upon this space – welcome! Let's embark on this journey
+        together, exploring the enchanting world enroute, and marveling at the
+        beauty that is the confluence of code and culture.
+      </p>
+
+      <p className="mb-2 text-lg">
+        To many adventures, both on-screen and off,
+        <br />
+        Adesewa.
+      </p>
+    </div>
+  );
 }
